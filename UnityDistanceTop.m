9c4cf302def564823aed8621b84b7a004057efd3
Peter Le <ptl7263@rit.edu>
2024-02-19 18:38:58 -0500

    Changed server script to modify pitch, roll, yaw of camera

--- UnityDistanceTop.m
+++ UnityDistanceTop.m
@@ -13,32 +13,30 @@ client = tcpclient(server_ip,server_port);
 fprintf(1,"Connected to server\n");
 
 % x,y,z,yaw[z],pitch[y],roll[x]
-depthRange_m = [-0.5:-0.1:-4.75];
-computedRange_mm = [];
-for i = 1:length(depthRange_m)
-    pose = [depthRange_m(i),-0.03,0,0,0,0];
-    [x1, y1] = getCenterPixels(depthRange_m(i), pose, client);
-    pose = [depthRange_m(i),0.03,0,0,0,0];
-    [x2, y2] = getCenterPixels(depthRange_m(i), pose, client);
-    [Z_mm, X_mm, Y_mm] = getCenterPosition(x1, y1, x2, y2);
-    Z_mm
-    computedRange_mm = [computedRange_mm, Z_mm];
-end
+x = -10;
+y = 13;
+z = 0;
+pitch = 55;
+roll = 0;
+yaw = 90;
+pose = [x,y,z,yaw,pitch,roll];
+unityImageLeft = unityLink(client,pose);
+subplot(1, 2, 1);
+imshow(unityImageLeft);
 
-computedRange_m = computedRange_mm / 1000;
-depthRange_m = -1 * depthRange_m;
 
-percentError = [];
-for i = 1 : length(depthRange_m)
-    percentError = [percentError, (computedRange_m(i) - depthRange_m(i))];
-end
+x2 = 10;
+y2 = 13;
+z2 = 0;
+pitch2 = 55;
+roll2 = 0;
+yaw2 = -90;
+pose2 = [x2,y2,z2,yaw2,pitch2,roll2]
+unityImageRight = unityLink(client,pose2);
+subplot(1, 2, 2);
+imshow(unityImageRight);
 
-figure
-plot(depthRange_m, percentError);
-grid on
-xlabel('Nominal Depth of Ball (m)');
-ylabel('Error = Computed - Actual (m)');
-title('Computed-Nominal Range Error vs Actual Tennis Ball');
+
 
 %Close Gracefully
 fprintf(1,"Disconnected from server\n");
