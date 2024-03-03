7b349d80d4f99ccd70960a70d89b008cd9ad4ebb
Peter Le <ptl7263@rit.edu>
2024-03-03 11:26:52 -0500

    Modified unityDistanceTop to do binarize demo

--- UnityDistanceTop.m
+++ UnityDistanceTop.m
@@ -13,29 +13,28 @@ client = tcpclient(server_ip,server_port);
 fprintf(1,"Connected to server\n");
 
 % x,y,z,yaw[z],pitch[y],roll[x]
-x = -8;
-y = 4.5;
-z = 14;
-pitch = 25;
+x = 0;
+y = 13.5;
+z = 0;
+pitch = 90;
 roll = 0;
-yaw = 137;
+yaw = 90;
 pose = [x,y,z,yaw,pitch,roll];
 unityImageLeft = unityLink(client,pose);
-subplot(1, 2, 1);
+%subplot(1, 2, 1);
 imshow(unityImageLeft);
+imsave;
 
-
-x2 = 8;
-y2 = 4.5;
-z2 = -14;
-pitch2 = 25;
-roll2 = 0;
-yaw2 = -43;
-pose2 = [x2,y2,z2,yaw2,pitch2,roll2]
-unityImageRight = unityLink(client,pose2);
-subplot(1, 2, 2);
-imshow(unityImageRight);
-
+%x2 = 8;
+%y2 = 4.5;
+%z2 = -14;
+%pitch2 = 25;
+%roll2 = 0;
+%yaw2 = -43;
+%pose2 = [x2,y2,z2,yaw2,pitch2,roll2];
+%unityImageRight = unityLink(client,pose2);
+%subplot(1, 2, 2);
+%imshow(unityImageRight);
 
 
 %Close Gracefully
