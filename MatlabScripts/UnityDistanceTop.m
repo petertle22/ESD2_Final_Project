bae72dff04e748283734793a5be728aa72b37e5f
Peter Le <ptl7263@rit.edu>
2024-03-28 17:17:22 -0400

    Found initial solution to camera problem with baseline = 100m, focal = 2.56 mm, pixelSize = 0.006 mm, and the camera is 9 meters above the ground

--- MatlabScripts/UnityDistanceTop.m
+++ MatlabScripts/UnityDistanceTop.m
@@ -14,30 +14,30 @@ fprintf(1,"Connected to server\n");
 
 % x,y,z,yaw[z],pitch[y],roll[x]
 x = 0;
-y = 4.2;
-z = -0.03;
+y = 9;
+z = -0.050;
 pitch = 90;
 roll = 0;
 yaw = 90;
 obj = 1; % 1 means camera, 2 means ball
 pose = [x,y,z,yaw,pitch,roll, obj];
 unityImageLeft = unityLink(client,pose);
-%subplot(1, 2, 1);
+subplot(1, 2, 1);
 imshow(unityImageLeft);
-imsave
+imwrite(unityImageLeft, 'left.jpg');
 
 x2 = 0;
-y2 = 4.2;
-z2 = 0.03;
+y2 = 9;
+z2 = 0.050;
 pitch2 = 90;
 roll2 = 0;
 yaw2 = 90;
 obj2 = 1;
 pose2 = [x2,y2,z2,yaw2,pitch2,roll2, obj2];
 unityImageRight = unityLink(client,pose2);
-%subplot(1, 2, 2);
+subplot(1, 2, 2);
 imshow(unityImageRight);
-imsave
+imwrite(unityImageRight, 'right.jpg');
 
 %Close Gracefully
 fprintf(1,"Disconnected from server\n");
