416983045ceeebcdb9be05b24375271107ca2bba
Peter Le <ptl7263@rit.edu>
2024-03-30 17:18:08 -0400

    Reorganized esd2 code repo

--- MatlabScripts/binarizeCameraDemo.m
+++ MatlabScripts/binarizeCameraDemo.m
@@ -1,17 +1,17 @@
 figure
-firstFrame = imread("testImages/leftImage1.jpg");
-%imshow(firstFrame)
+firstFrame = imread("testImages/left1_1.jpg");
+imshow(firstFrame)
 
 figure
-secondFrame = imread("testImages/leftImage2.jpg");
+secondFrame = imread("testImages/left2_1.jpg");
 imshow(secondFrame)
 
 
+figure
 diff = secondFrame - firstFrame;
-%imshow(diff)
+imshow(diff)
 
 grayDiff = rgb2gray(diff);
-%imshow(grayDiff)
 
 thresholdValue = 10;
 binaryDiff = imbinarize(grayDiff, thresholdValue/255);
