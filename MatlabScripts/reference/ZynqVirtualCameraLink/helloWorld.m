ed395591078724113745bc267829262edb445023
Daniel Gebura (RIT Student) <djg2170@rit.edu>
2024-04-10 11:22:38 -0400

    ZynqVirtualCamLink altered

--- MatlabScripts/reference/ZynqVirtualCameraLink/helloWorld.m
+++ MatlabScripts/reference/ZynqVirtualCameraLink/helloWorld.m
@@ -3,11 +3,17 @@
 % must run helloWorld.py first on the FPGA SoC
 
 %Initialization Parameters
-server_ip   = '129.21.70.165';     % IP address of the server
+server_ip   = '129.21.40.214';     % IP address of the server
 server_port = 9999;                % Server Port of the sever
 
 client = tcpclient(server_ip,server_port);
 fprintf(1,"Connected to server\n");
  
-write(client,'0');
-flush(client);
+for i = 5:6
+    % Convert the current number i to a string and concatenate it
+    numberStr = num2str(i);
+    
+    % Send the string to the client
+    write(client, numberStr);
+    flush(client);
+end
