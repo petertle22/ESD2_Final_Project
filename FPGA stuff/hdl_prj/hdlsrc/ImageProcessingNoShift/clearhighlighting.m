b39507e547a548b71b7d446b4e075e722d646574
Jake Stein (RIT Student) <jms7664@rit.edu>
2024-04-24 10:34:57 -0400

    adding fpga stuff

---
+++ FPGA stuff/hdl_prj/hdlsrc/ImageProcessingNoShift/clearhighlighting.m
@@ -0,0 +1,10 @@
+SLStudio.Utils.RemoveHighlighting(get_param('ImageProcessingNoShift', 'handle'));
+SLStudio.Utils.RemoveHighlighting(get_param('gm_ImageProcessingNoShift', 'handle'));
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1', 1, 1, '');
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1/Binarize', 0, 1, '');
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1/Delay2', 1, 1, '');
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1/Bit Concat', 0, 1, '');
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1/Sum', 0, 1, '');
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1/gray', 0, 1, '');
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1/Binarize/Compare To Constant', 0, 1, '');
+annotate_port('gm_ImageProcessingNoShift/subtraction_core/Subsystem1/Binarize/Index Vector', 0, 1, '');
