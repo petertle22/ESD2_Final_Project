5a3ef6404b76c905ed27209a70c83249ad174a43
Daniel-Gebura <153743516+Daniel-Gebura@users.noreply.github.com>
2024-04-29 21:51:14 -0400

    Figure Capture Server/Client

---
+++ MatlabScripts/Demo/MatlabClient_FIGURES.m
@@ -0,0 +1,269 @@
+% MatlabClient_Final performs the following:
+% - Waits for the GUI to initialize its parameters
+% - Once GUI initiates process, connects to server over wifi
+% - Sends INIT PARAMETERS protocol to server
+% - Enters REQUEST-RETRIEVE-RESPOND LOOP to send frames to the server
+% - Once told to stop, requests results
+% - Receives results to diaply on GUI including some debugging graphics
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CONSTANTS AND SETTINGS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Image Transfer Dimensions
+width = 752.0;
+height = 480.0;
+
+% Server IP address
+%server_ip   = '129.21.42.146'; % Debug Computer
+server_ip   = '129.21.42.10'; % Board
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CLIENT INITIALIZATION (GUI PASS PARAMETERS/START)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Function to preprocess images (convert to grayscale)
+preprocessImage = @(img) rgb2gray(img);
+% Get and save Empty Court images (Left/Right) ONCE. 
+emptyLeftImage = imread("../../testImages/LeftEmptyCourt.jpg");
+emptyRightImage = imread("../../testImages/rightEmptyCourt.jpg");
+emptyLeftGray = preprocessImage(emptyLeftImage);
+emptyRightGray = preprocessImage(emptyRightImage);
+
+% Get these parameters from GUI
+MODE = '2';       % 1 -> coefficient of restitution, 2 -> ball is inside or out
+MATCH_TYPE = '1'; % 1 -> singles mode, -> 2 doubles mode
+SHOT_TYPE = '1';  % 1 -> serve mode, 2 -> volley mode
+path = '../datFiles/serve1.dat'; % Path to .dat file being used
+ITERATION = 1;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CONNECT TO SERVER
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Connect to sever;
+server_port = 9999;                % Server Port of the sever
+client = tcpclient(server_ip,server_port);
+fprintf(1,"Connected to server\n");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SEND SERVER INIT PARAMETERS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Tell server to prepare to receive init parameters
+write(client,'0'); %Transfer Protocol
+flush(client);
+
+% Send mode
+write(client, MODE);
+flush(client);
+% Send match type
+write(client, MATCH_TYPE);
+flush(client);
+% Send shot type
+write(client, SHOT_TYPE);
+flush(client);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% REQUEST-RETRIEVE-RESPOND LOOP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Tell Server to begin processing a shot
+write(client,'1'); %Transfer Protocol
+flush(client);
+
+% Loop until Server sends special value
+while 1
+    % 1. REQUEST - Wait for request from server (t value), exit if special value
+    request = read(client, 1, 'uint32');
+    request;
+    if request == 99999 % Check if received STOP CMD
+        break % Leaving Request-retrieve-respond Loop
+    end
+
+    % 2. RETRIEVE - Get Left/Right Unity images at time t
+    errorCode = MoveTennisBall(request, path); % Move Tennis ball to desired location based on .dat file
+    % Check if a request frame is valid (shot hasn't ended yet)
+    if errorCode == 0 % Valid request: Send the frame
+        % Get the image from UNITY
+        [leftImage, rightImage] = MoveCamera(request, 0);
+        % SAVE FIGURE 1
+        if (ITERATION == 30)
+            imwrite(leftImage, 'UNITY_Image.jpg');
+        end
+        % Grayscale Left/Right
+        BallLeftGray  = preprocessImage(leftImage);
+        % SAVE FIGURE 2
+        if (ITERATION == 30)
+            imwrite(BallLeftGray, 'MATLAB_GRAY_Image.jpg');
+        end
+        BallRightGray = preprocessImage(rightImage);
+        % Package all four images
+        imageStack = uint8(ones(height,width,8));
+        imageStack(:,:,1) = BallLeftGray;
+        imageStack(:,:,2) = emptyLeftGray;
+        imageStack(:,:,3) = BallRightGray;
+        imageStack(:,:,4) = emptyRightGray;
+        imageStack(:,:,5) = emptyRightGray;
+        imageStack(:,:,6) = BallRightGray;
+        imageStack(:,:,7) = emptyLeftGray;
+        imageStack(:,:,8) = BallLeftGray;
+    else % Invalid request: Shot ended. Send dummy frames
+        %Package all four images (Empty Frames)
+        disp('Requested Invalid Frames')
+        imageStack = uint8(ones(height,width,8));
+        imageStack(:,:,1) = ones(480, 752) * 0;
+        imageStack(:,:,2) = ones(480, 752) * 0;
+        imageStack(:,:,3) = ones(480, 752) * 0;
+        imageStack(:,:,4) = ones(480, 752) * 0;
+        imageStack(:,:,5) = ones(480, 752) * 0;
+        imageStack(:,:,6) = ones(480, 752) * 0;
+        imageStack(:,:,7) = ones(480, 752) * 0;
+        imageStack(:,:,8) = ones(480, 752) * 0;
+    end
+    
+    % 3. RESPOND - Send retrieved image stack
+    imageStack = permute(imageStack,[3 2 1]);
+    write(client,imageStack(:)); %SEND
+    flush(client);
+    ITERATION = ITERATION +1;
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% RECEIVE RESULTS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Tell server to send requested results
+write(client,'2'); %Transfer Protocol
+flush(client);
+
+% RECEIVE FIGURE 4
+Fig4NumFrames = read(client, 1, 'uint32');
+Fig4XValues = read(client,numFrames, 'double');
+Fig4YValues = read(client,numFrames, 'double');
+Fig4ZValues = read(client,numFrames, 'double');
+Fig4TValues = read(client,numFrames, 'uint32');
+
+% RECEIVE FIGURE 5/6
+Fig5NumFrames = read(client, 1, 'uint32');
+Fig5XValues = read(client,numFrames, 'double');
+Fig5YValues = read(client,numFrames, 'double');
+Fig5ZValues = read(client,numFrames, 'double');
+Fig5TValues = read(client,numFrames, 'uint32');
+
+% Verify result format
+receiveMode = read(client, 1, 'uint32');
+
+% Get Mode-Specific Results
+if receiveMode == 1 % Coefficient of restitution
+    restitutionCoeff = read(client, 1, 'double');
+
+else % In/Out Decison or DEBUGGING
+    if receiveMode == 2 % In/Out decision
+        isShotIn = read(client, 1, 'double'); % 1.0 = TRUE, 0.0 = FALSE
+    end 
+
+    % Receive calculated X,Y,Z trajectories
+    xCoeff = read(client, 2, 'double');
+    yCoeff = read(client, 2, 'double');
+    zLen = read(client, 1, 'uint32');
+    zCoeff = read(client, zLen, 'double');
+    bounceT = read(client, 1, 'double');
+end
+
+% CLOSE SERVER
+write(client,'9999');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FORMAT RESULTS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% IMPLEMENTATION NOTE:
+% - Prep top down plot of court as described below
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GUI RESULTS DISPLAY
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% IMPLEMENTATION NOTE: 
+% - If Coeff mode, gui only needs to state coefficient
+%   of restitution
+% - If In/Out Mode, gui displays In/Out decison and shows a top down plot
+%   of the court with the calc X,Y trajectory plotted ontop. Then put a
+%   special marker on he line at bounce(x,y) [x and y trajectories evaluated
+%   at bounceT]
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ADDITIONAL INFO DISPLAY
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if receiveMode == 2
+    % FIGURE 4 - Unfiltered Data
+    % Populate actual correct results
+    ballData = load(path);
+    real_X = NaN(1, length(Fig4TValues));
+    real_Y = NaN(1, length(Fig4TValues));
+    real_Z = NaN(1, length(Fig4TValues));
+    for i = 1:length(Fig4TValues)
+        if i <= size(ballData, 1)
+            real_X(i) = ballData(i, 3);
+            real_Y(i) = ballData(i, 1);
+            real_Z(i) = ballData(i, 2);
+        else
+            error('Index exceeds the number of rows in ballData file.');
+        end
+    end
+    % Plot Figure 4
+    figure;
+    hold on;  % This command keeps the current plot and all axes properties so that the next plot adds to it.
+    plot(Fig4TValues, real_Z, 'b-', 'LineWidth', 2);  % Plot real_Z in blue with a line width of 2
+    plot(Fig4TValues, Fig4ZValues, 'r-', 'LineWidth', 2);  % Plot Fig4ZValues in red  line with a line width of 2
+    % Adding labels and legend
+    xlabel('Time (ms)');
+    ylabel('Z Values, m');
+    title('Comparison of Real Z Values and Unfiltered Z Values Over Time');
+    legend('Real Z Values', 'Unfiltered Z Values');
+    hold off;
+    % Save the figure to a JPEG file
+    saveas(gcf, 'UNFILTERED_Z_VALUES.jpg');
+
+
+    % FIGURE 5 - Filtered Data
+    % Populate actual correct results
+    ballData = load(path);
+    real_X = NaN(1, length(Fig5TValues));
+    real_Y = NaN(1, length(Fig5TValues));
+    real_Z = NaN(1, length(Fig5TValues));
+    for i = 1:length(Fig5TValues)
+        if i <= size(ballData, 1)
+            real_X(i) = ballData(i, 3);
+            real_Y(i) = ballData(i, 1);
+            real_Z(i) = ballData(i, 2);
+        else
+            error('Index exceeds the number of rows in ballData file.');
+        end
+    end
+    % Plot Figure 5
+    figure;
+    hold on;  % This command keeps the current plot and all axes properties so that the next plot adds to it.
+    plot(Fig5TValues, real_Z, 'b-', 'LineWidth', 2);  % Plot real_Z in blue with a line width of 2
+    plot(Fig5TValues, Fig5ZValues, 'r-', 'LineWidth', 2);  % Plot Fig5ZValues in red  line with a line width of 2
+    % Adding labels and legend
+    xlabel('Time (ms)');
+    ylabel('Z Values, m');
+    title('Comparison of Real Z Values and Filtered Z Values Over Time');
+    legend('Real Z Values', 'Filtered Z Values');
+    hold off;
+    % Save the figure to a JPEG file
+    saveas(gcf, 'FILTERED_Z_VALUES.jpg');
+
+
+    % 2. 3D plot of real and calc trajectory?
+    % Plotting
+    figure;
+    plot3(Fig5XValues, Fig5YValues, Fig5ZValues, 'r'); % Plot calculated X,Y,Z positions in red
+    hold on;
+    plot3(real_X, real_Y, real_Z, 'b'); % Plot real X,Y,Z positions in blue
+    legend('Calculated XYZ', 'Real XYZ');
+    title('Comparison of Calculated and Real XYZ Positions Over Time');
+    xlabel('X position, m');
+    ylabel('Y position, m');
+    zlabel('Z position, m');
+    grid on;
+    zlim([0 inf]); % Set the minimum Z value to 0
+    hold off;
+    saveas(gcf, 'FILTERED_DATA_3D.jpg', 'jpg');
+
+end
---
+++ pythonCode/Demo/ZynqServer_FIGURES.py
@@ -0,0 +1,203 @@
+"""
+ZynqServer_FIGURES.py
+"""
+print("File Opened")
+from numpysocket import NumpySocket
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+import time
+import mmap
+import struct
+import sys, random
+import ctypes
+import copy
+import TCP_communication as tcp
+import ballDetection as ball
+import frameGrabber as fpga
+
+# CONSTANTS
+#----------------------------------------------------------------------------------------------------------
+# TCP Commands
+STOP_CMD = 99999
+INIT_CMD = 0
+PROCESS_CMD = 1
+RESULTS_CMD = 2
+
+# General Mapping
+MODE_COEFF = 1
+MODE_IN_OUT = 2
+MATCH_TYPE_SINGLES = 1
+MATCH_TYPE_DOUBLES = 2
+SHOT_TYPE_SERVE = 1
+SHOT_TYPE_VOLLEY = 2
+
+# SETTINGS
+FPGA_ENABLE = False
+CV2_PREPROCESS_ENABLE = True
+WINDSHIFT_ENABLE = False
+ACCEL_PROCESSING = True
+FRAME_REQUEST_TIMEOUT = 2000
+T_SKIP = 20
+PROCESS_T = 3
+FIXED_PROCESS_TIME = True
+
+ITERATION = 1
+#----------------------------------------------------------------------------------------------------------
+# INITIALIZE FPGA
+if (FPGA_ENABLE):
+    print("Initializing FPGA...")
+    camProcessed, camFeedthrough, camWriter = fpga.initFPGA()
+    print("Initialized.")
+
+while(True):
+    # Open Server
+    npSocket = NumpySocket()
+    npSocket.startServer(9999)
+    print("Server Started")
+
+    # ENTER INFINITE MAIN LOOP
+    while True:
+        # TOP TRANSFER PROTOCOL MANAGEMENT
+        print('Awaiting cmd...')
+        cmd = int(npSocket.receiveCmd())  # Await Transfer Protocol cmd
+        if cmd == INIT_CMD: # INIT PARAMETERS
+            # Get init parameters from client
+            mode, matchType, shotType = tcp.getInitParameters(npSocket)
+
+        elif cmd == PROCESS_CMD: # Process Shot
+            # Initialize Variables
+            resultsReady = False
+            t = 1  # initialize to start of shot
+            frame = 0  # current frame counter
+            ballPositionXYZ = np.zeros((4, 0), dtype=int)  # Initialize a 2D array with 4 rows and dynamic columns
+
+            # SPEED OPTIMIZATION
+            dummyFrame = np.zeros((480, 752), dtype=np.uint8)  # Initialize an empty dummy image
+            _, _, _ = ball.find_centroid(dummyFrame)
+
+            # Process All Frames
+            while True:  # While more frames to process
+                # Get images at time t
+                if (t > FRAME_REQUEST_TIMEOUT): # Check for invalid t request
+                    print("All Frames Received")
+                    break
+                tcp.requestFrame(t, frame, npSocket) # Send request for frame at t
+                if(not FPGA_ENABLE):
+                    frame_data = tcp.receiveFrame(npSocket) # Receive frame for t
+                    # Access the individual images from frame
+                    ballLeftGray = frame_data['ballLeftGray']
+                    emptyLeftGray = frame_data['emptyLeftGray']
+                    ballRightGray = frame_data['ballRightGray']
+                    emptyRightGray = frame_data['emptyRightGray']
+                else:
+                    data = npSocket.receive() # Read data from client
+
+                # Check if shot has ended
+                if np.all(emptyLeftGray == 0) and np.all(emptyRightGray == 0):
+                    # Stop processing frames
+                    print("Requested for frame out of bounds for time ", t)
+                    break
+
+                # Start Processing Current Frame
+                start_time = time.time()  # start a timer from 0 to track processing time
+                # 0. Adapt stereo background to possible wind shifts in new stereo image
+                if (WINDSHIFT_ENABLE): # Wind present, may shift camera around
+                    # IMPLEMENT 
+                    # Account for a winshift in teh new ball images
+                    pass
+                else: # No processing needed. Images are perfectly alligned
+                    pass
+
+                # 1. Process frames to isolate ball from background
+                if (CV2_PREPROCESS_ENABLE): # Background subtract in cv2
+                    processedLeft, processedRight = ball.process_images(ballLeftGray, emptyLeftGray, ballRightGray, emptyRightGray)
+                elif (FPGA_ENABLE): # Processing needed by FPGA
+                    camWriter.setFrame(data) # Send stereo image for processing
+                    processedLeft,processedRight = camProcessed.getStereoGray() # Receieve processed stereo frames
+                    processedLeft = np.ascontiguousarray(processedLeft, dtype=np.uint8)
+                    processedRight = np.ascontiguousarray(processedRight, dtype=np.uint8)
+                else: # No processing needed. Server Initially receieved processed images
+                    processedLeft = ballLeftGray # => processedLeft = channel_0 image from client stream
+                    processedRight = emptyLeftGray # => processedRight = channel_1 image from client stream
+
+                # SAVE FIGURE 3
+                if (ITERATION == 30):
+                    # Save processed Left
+                    cv2.imwrite('SERVER_PROCESSED_Image.jpg', processedLeft)
+                # 2. Centroid Detection
+                ballFound, xLeft, yLeft = ball.find_centroid(processedLeft)
+                ballFound, xRight, yRight = ball.find_centroid(processedRight)
+
+                # 3. Stereo Calculate X,Y,Z at t
+                if ((t > 5) and (ballFound)): # Only Calculate for frames after initialization and with a found ball
+                    X, Y, Z = ball.calcStereoXYZ(xLeft, yLeft, xRight, yRight)
+                    newPosition = np.array([[X], [Y], [Z], [t]])
+                    ballPositionXYZ = np.hstack((ballPositionXYZ, newPosition))  # Append new frame data as a new column
+                    frame += 1  # Increment frame counter
+
+                # 4. Update t
+                end_time = time.time()
+                if ((not ballFound) and (ACCEL_PROCESSING)): # DEBUGGING update t faster when ball is out of frame
+                    t += T_SKIP
+                elif(FIXED_PROCESS_TIME):
+                    t += PROCESS_T
+                else:
+                    t += int((end_time - start_time) * 1000)  # Convert processing time to ms
+
+            # All Frames Processed
+            print("Exiting process frames loop")
+            resultsReady = True
+            tcp.sendCMD(STOP_CMD, npSocket)  # Stop Command: Tell Client to stop sending frames and instead request the result back
+            print('Sent Stop CMD')
+
+        elif cmd == RESULTS_CMD: # Send Results
+            print('Results Requested...')
+
+            # Calculate Corresponding Result
+            if (resultsReady):
+                # SEND FIGURE 4. Unfiltered Data 
+                tcp.sendBallXYZ(ballPositionXYZ)
+                # Remove any completely inaccurate data
+                ballPositionXYZ = ball.removeInvalidXYZ(ballPositionXYZ)
+
+                # Get/Send Mode-Specific Results
+                if (mode == MODE_COEFF):  # Coeff Mode
+                    print('Calculating Coefficient of Restitution...')
+                    # Just calculate coefficient of restitution and send
+                    beforeBounceXYZ, afterBounceXYZ = ball.filterStereoXYZ_Coeff(ballPositionXYZ) # Filter 
+                    coeff = ball.getCoefficientOfRestitution(beforeBounceXYZ, afterBounceXYZ) # Calculate coefficient
+                    tcp.sendResult(mode, coeff, npSocket) # Send Coefficient
+
+                elif mode == MODE_IN_OUT:  # Shot Mode
+                    print('Calculating In/Out...')
+                    ballPositionXYZ = ball.filterStereoXYZ(ballPositionXYZ, shotType)
+                    # SEND FIGURE 5. Filtered Data 
+                    tcp.sendBallXYZ(ballPositionXYZ)
+                    lineDecision, _, _ = ball.getLineDecision(ballPositionXYZ, matchType, shotType) # Calculate In/Out
+                    tcp.sendResult(mode, lineDecision, npSocket) # Send In/Out
+                    # Get and send trajectory of shot
+                    bounceX, bounceY, bounceZ = ball.getBallTrajectory(ballPositionXYZ)
+                    tcp.sendTrajectoryCoeff(bounceX, bounceY, bounceZ, ball.findBounceT(ballPositionXYZ), npSocket)
+                    # Report bounceT to user
+                    print('Bounce t:')
+                    print(ball.findBounceT(ballPositionXYZ))
+
+                else:  # DEBUGGING MODE
+                    print('DEBUGGING RESULTS...')
+                    # Send XYZ over t Information
+                    ballPositionXYZ = ball.filterStereoXYZ(ballPositionXYZ, shotType)
+                    bounceX, bounceY, bounceZ = ball.getBallTrajectory(ballPositionXYZ)
+                    tcp.sendTrajectoryCoeff(bounceX, bounceY, bounceZ, ball.findBounceT(ballPositionXYZ), npSocket)
+                    print('Bounce t:')
+                    print(ball.findBounceT(ballPositionXYZ))
+
+                print('Results Sent')
+            # Client has requested results at an invalid time
+            else :
+                print('Results Not Valid')
+
+        else: # Received Terminate Command
+            print("Exit Command. Close Server")
+            npSocket.close()  # Close Server
+            break  # Break out of the loop for any other value
